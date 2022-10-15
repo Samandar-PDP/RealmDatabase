cb57982ec162ee175c24e5381e82de791b930b39
samandar-mee <muhammaddostonbek@gmail.com>
2022-10-15 16:48:41 +0500

    init

---
+++ app/src/main/java/com/example/realmdatabase/MyApp.kt
@@ -0,0 +1,19 @@
+package com.example.realmdatabase
+
+import android.app.Application
+import android.util.Log
+import io.realm.Realm
+import io.realm.RealmConfiguration
+
+class MyApp: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Realm.init(this)
+        val configuration = RealmConfiguration.Builder()
+            .allowWritesOnUiThread(true)
+            .deleteRealmIfMigrationNeeded()
+            .schemaVersion(1)
+            .build()
+        Realm.setDefaultConfiguration(configuration)
+    }
+}
---
+++ app/src/main/java/com/example/realmdatabase/model/Note.kt
@@ -0,0 +1,19 @@
+package com.example.realmdatabase.model
+
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+
+open class Note : RealmObject {
+    @PrimaryKey
+    var id: Long = 0
+    var time: String? = null
+    var note: String? = null
+
+    constructor()
+
+    constructor(id: Long, time: String?, note: String?) : super() {
+        this.id = id
+        this.time = time
+        this.note = note
+    }
+}
